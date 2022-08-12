4672f1c12a8bdcf3469d2fc80cc6f991e78c8078
Aymen <AymenMir1001@gmail.com>
2022-08-12 23:48:48 +0100

    Update Footer.js

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -5,16 +5,16 @@ import { RiDiscordFill } from "react-icons/ri";
 function Footer() {
     return (
         <div className='footer container'>
-            <p>&#169; All Right Reserved</p>
+            <p>KryptoPunks&#169; All Right Reserved</p>
             <div className='social'>
                 <a href='https://github.com/Aymen1001' >
-                    <AiOutlineGithub size={25} color="#000" />
+                    <AiOutlineGithub size={24} color="#000" />
                 </a>
                 <a href='https://github.com/Aymen1001' >
-                    <AiOutlineTwitter size={25} color="#000" />
+                    <AiOutlineTwitter size={24} color="#000" />
                 </a>
                 <a href='https://github.com/Aymen1001' >
-                    <RiDiscordFill size={25} color="#000" />
+                    <RiDiscordFill size={24} color="#000" />
                 </a>
             </div>
         </div>
